d8b1bb09c2a3ac94f45266e003b9dc2ad13c7136
Aleksey <reporn05@gmail.com>
2020-03-19 08:26:06 +0300

    fix exception

---
+++ IGI.lab1/src/DataAccess/Repository/CsvRepository.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using DataAccess.Interfaces;
+
+namespace DataAccess.Repository
+{
+    public class CsvRepository<T> : IRepository<T>
+    {
+        private string _connectionString;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<CsvRepository<T>> _logger;
+        private readonly ClassMap<T> _map;
+
+        public CsvRepository(IConfiguration configuration,
+            ILogger<CsvRepository<T>> logger,
+            ClassMap<T> map = null
+            )
+        {
+            _configuration = configuration;
+            _logger = logger;
+            _map = map;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            var entities = ReadFile();
+
+            return entities;
+        }
+
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ConnectionString
+        {
+           get
+           {
+               if (string.IsNullOrEmpty(_connectionString))
+               {
+                   _connectionString = _configuration.GetConnectionString("CSV");
+               }
+
+               return _connectionString;
+           }
+        }
+
+        private IEnumerable<T> ReadFile()
+        {
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                HeaderValidated = null,
+                TrimOptions = TrimOptions.Trim,
+            };
+
+
+            using var reader = new StreamReader(ConnectionString);
+            using var csvReader = new CsvReader(reader,configuration);
+
+            csvReader.Configuration.ReadingExceptionOccurred = ExceptionHandler;
+
+            if (_map != null)
+            {
+                csvReader.Configuration.RegisterClassMap(_map);
+            }
+
+            var list = new List<T>();
+
+            try
+            {
+                list = csvReader.GetRecords<T>().ToList();
+            }
+            catch
+            {
+                return list;
+            }
+
+            return list;
+        }
+
+        private bool ExceptionHandler(CsvHelperException exception)
+        {
+            _logger.LogError(exception.Message);
+            throw exception;
+        }
+
+
+    }
+}
---
+++ IGI.lab1/src/FileConverter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using BusinessLogic.Map;
+using BusinessLogic.Services;
+using CommandLine;
+using CsvHelper.Configuration;
+using DataAccess.Interfaces;
+using DataAccess.Models;
+using DataAccess.Repository;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace FileConverter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true);
+
+            var config = configBuilder.Build();
+
+            var services = new ServiceCollection()
+                .AddSingleton(typeof(IRepository<Students>), typeof(CsvRepository<Students>))
+                .AddSingleton(typeof(ClassMap<Students>), typeof(Mapping))
+                .AddSingleton<IConfiguration>(config)
+                .AddSingleton<ReportCreatorServices>()
+                .AddTransient<ReportServices>()
+                .AddLogging(log =>
+                {
+                    log.AddFile("Logs/log-{Date}.txt");
+                    log.AddConsole();
+                })
+                .BuildServiceProvider();
+
+
+
+            var logger = services.GetService<ILoggerFactory>().CreateLogger<Program>();
+            
+            logger.LogInformation("Starting application");
+
+            AppStart(services.GetService<ReportServices>(), args);
+            
+            logger.LogInformation("All done!");
+        }
+
+        private static void AppStart(ReportServices service, string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    service.ChooseReportType(Enum.Parse<ReportType>(o.ReportType));
+                });
+        }
+
+        // ReSharper disable once ClassNeverInstantiated.Local
+        private class Options
+        {
+            [Option('t', "report type", Required = true, HelpText = "Set output for report type. 1 is json, 2 is excel")]
+            public string ReportType { get; set; }
+        }
+    }
+
+}
